70cc199ff60bd13197bb1bda90d00f9578d8c660
Yashas Reddy <39880118+YashasReddy@users.noreply.github.com>
2020-10-31 08:43:18 +0530

    Update app.js

--- scripts/app.js
+++ scripts/app.js
@@ -1,54 +1,47 @@
-const cityForm = document.querySelector('form');
-const card = document.querySelector('.card');
-const details = document.querySelector('.details');
-const time = document.querySelector('img.time');
-const icon = document.querySelector('.icon img');
+"use strict";
 
-const updateUI = (data) => {
+var cityForm = document.querySelector('form');
+var card = document.querySelector('.card');
+var details = document.querySelector('.details');
+var time = document.querySelector('img.time');
+var icon = document.querySelector('.icon img');
+
+var updateUI = function updateUI(data) {
   // destructure properties
-  const { cityDets, weather } = data;
+  var cityDets = data.cityDets,
+      weather = data.weather; // update details template
 
-  // update details template
-  details.innerHTML = `
-    <h5 class="my-3">${cityDets.EnglishName}</h5>
-    <div class="my-3">${weather.WeatherText}</div>
-    <div class="display-4 my-4">
-      <span>${weather.Temperature.Metric.Value}</span>
-      <span>&deg;C</span>
-    </div>
-  `;
+  details.innerHTML = "\n    <h5 class=\"my-3\">".concat(cityDets.EnglishName, "</h5>\n    <div class=\"my-3\">").concat(weather.WeatherText, "</div>\n    <div class=\"display-4 my-4\">\n      <span>").concat(weather.Temperature.Metric.Value, "</span>\n      <span>&deg;C</span>\n    </div>\n  "); // update the night/day & icon images
 
-  // update the night/day & icon images
-  const iconSrc = `img/icons/${weather.WeatherIcon}.svg`;
+  var iconSrc = "img/icons/".concat(weather.WeatherIcon, ".svg");
   icon.setAttribute('src', iconSrc);
-  
-  const timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
-  time.setAttribute('src', timeSrc);
+  var timeSrc = weather.IsDayTime ? 'img/day.svg' : 'img/night.svg';
+  time.setAttribute('src', timeSrc); // remove the d-none class if present
 
-  // remove the d-none class if present
-  if(card.classList.contains('d-none')){
+  if (card.classList.contains('d-none')) {
     card.classList.remove('d-none');
   }
 };
 
-const updateCity = async (city) => {
-
-  const cityDets = await getCity(city);
-  const weather = await getWeather(cityDets.Key);
-  return { cityDets, weather };
-
+var updateCity = async function updateCity(city) {
+  var cityDets = await getCity(city);
+  var weather = await getWeather(cityDets.Key);
+  return {
+    cityDets: cityDets,
+    weather: weather
+  };
 };
 
-cityForm.addEventListener('submit', e => {
+cityForm.addEventListener('submit', function (e) {
   // prevent default action
-  e.preventDefault();
-  
-  // get city value
-  const city = cityForm.city.value.trim();
-  cityForm.reset();
+  e.preventDefault(); // get city value
 
-  // update the ui with new city
-  updateCity(city)
-    .then(data => updateUI(data))
-    .catch(err => console.log(err));
+  var city = cityForm.city.value.trim();
+  cityForm.reset(); // update the ui with new city
+
+  updateCity(city).then(function (data) {
+    return updateUI(data);
+  }).catch(function (err) {
+    return console.log(err);
+  });
 });

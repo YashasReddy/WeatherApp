4401c5b8787009a5f0e309c6a0985038cb51d3a7
Yashas Reddy <39880118+YashasReddy@users.noreply.github.com>
2020-11-10 12:43:37 +0530

    Update forecast.js

--- scripts/forecast.js
+++ scripts/forecast.js
@@ -1,20 +1,27 @@
-"use strict";
+const key = '0maXh2wUkHGY5GllqfBKSKrVKp0IkckH';
 
-var key = '0maXh2wUkHGY5GllqfBKSKrVKp0IkckH'; // get weather information
+// get weather information
+const getWeather = async (id) => {
 
-var getWeather = async function getWeather(id) {
-  var base = 'http://dataservice.accuweather.com/currentconditions/v1/';
-  var query = "".concat(id, "?apikey=").concat(key);
-  var response = await fetch(base + query);
-  var data = await response.json();
+  const base = 'https://dataservice.accuweather.com/currentconditions/v1/';
+  const query = `${id}?apikey=${key}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
   return data[0];
-}; // get city information
 
+};
 
-var getCity = async function getCity(city) {
-  var base = 'http://dataservice.accuweather.com/locations/v1/cities/search';
-  var query = "?apikey=".concat(key, "&q=").concat(city);
-  var response = await fetch(base + query);
-  var data = await response.json();
+// get city information
+const getCity = async (city) => {
+
+  const base = 'https://dataservice.accuweather.com/locations/v1/cities/search';
+  const query = `?apikey=${key}&q=${city}`;
+
+  const response = await fetch(base + query);
+  const data = await response.json();
+
   return data[0];
+
 };
